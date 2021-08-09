c258048ba1642dae6db0c818b28ef32d9563bf36
devmateusramos <devmateusramos@gmail.com>
2021-08-09 11:11:26 -0300

    limitando movimentação do helicopetero

--- construindo primeiro jogo de naves/jogo feito por mim/js/js.js
+++ construindo primeiro jogo de naves/jogo feito por mim/js/js.js
@@ -10,7 +10,26 @@ function start() { // Inicio da função start()
     //Principais variáveis do jogo
 	
 	var jogo = {}
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+        }
+	//pode olhar arquivo keys pra ver esses códigos
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla	
 	
+	$(document).keydown(function(e){
+        jogo.pressionou[e.which] = true;
+        });
+    
+    
+        $(document).keyup(function(e){
+           jogo.pressionou[e.which] = false;
+        });
+
+
 	//Game Loop
 
 	jogo.timer = setInterval(loop,30);
@@ -18,6 +37,7 @@ function start() { // Inicio da função start()
 	function loop() {
 	
 	movefundo();
+    movejogador();
 	
 	} // Fim da função loop()
     
@@ -29,5 +49,34 @@ function start() { // Inicio da função start()
         $("#fundoGame").css("background-position",esquerda-10);
         
         } // fim da função movefundo()
+
+        function movejogador() {
+	
+            if (jogo.pressionou[TECLA.W]) {
+                var topo = parseInt($("#jogador").css("top"));
+                $("#jogador").css("top",topo-10);
+                if (topo<=0) {
+		
+                    $("#jogador").css("top",topo+10);
+                }
+            
+            }
+            
+            if (jogo.pressionou[TECLA.S]) {
+                
+                var topo = parseInt($("#jogador").css("top"));
+                $("#jogador").css("top",topo+10);
+                if (topo>=434) {	
+                    $("#jogador").css("top",topo-10);
+                        
+                }	
+            }
+            
+            if (jogo.pressionou[TECLA.D]) {
+                
+                //Chama função Disparo	
+            }
+        
+            } // fim da função movejogador()
 
 } // Fim da função start
