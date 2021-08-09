315329358d4fcddb1f4754dda3fe3599f47b38ed
devmateusramos <devmateusramos@gmail.com>
2021-08-09 11:35:39 -0300

    disparo

--- construindo primeiro jogo de naves/jogo feito por mim/js/js.js
+++ construindo primeiro jogo de naves/jogo feito por mim/js/js.js
@@ -10,6 +10,7 @@ function start() { // Inicio da função start()
     //Principais variáveis do jogo
 	
 	var jogo = {}
+    var podeAtirar = true;
     var velocidade=5;
     var posicaoY = parseInt(Math.random() * 334);
     var TECLA = {
@@ -78,7 +79,8 @@ function start() { // Inicio da função start()
             }
             
             if (jogo.pressionou[TECLA.D]) {
-                
+
+                disparo();
                 //Chama função Disparo	
             }
         
@@ -121,5 +123,38 @@ function start() { // Inicio da função start()
                 }
         
         } // fim da função moveamigo()
+
+        function disparo() {
+	
+            if (podeAtirar==true) {
+                
+            podeAtirar=false;
+            
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX= parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro=topo+37;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top",topoTiro);
+            $("#disparo").css("left",tiroX);
+            
+            var tempoDisparo=window.setInterval(executaDisparo, 30);
+            
+            } //Fecha podeAtirar
+         
+                   function executaDisparo() {
+                posicaoX = parseInt($("#disparo").css("left"));
+                $("#disparo").css("left",posicaoX+15); 
+        
+                        if (posicaoX>900) {
+                                
+                    window.clearInterval(tempoDisparo);
+                    tempoDisparo=null;
+                    $("#disparo").remove();
+                    podeAtirar=true;
+                            
+                           }
+            } // Fecha executaDisparo()
+        } // Fecha disparo()
 
 } // Fim da função start
